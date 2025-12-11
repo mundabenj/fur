285bd4f8cd8d9df4310fe828eef9ff42d1eb3c69
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2025-12-11 12:54:17 +0300

    added triggers

--- fur.sql
+++ fur.sql
@@ -1,36 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 6.0.0-dev
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Dec 10, 2025 at 10:52 AM
--- Server version: 11.7.2-MariaDB
--- PHP Version: 8.3.25
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fur`
---
-DROP DATABASE IF EXISTS `fur`;
-CREATE DATABASE IF NOT EXISTS `fur` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `fur`;
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               12.0.2-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.11.0.7065
 -- --------------------------------------------------------
 
---
--- Table structure for table `gender`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP TABLE IF EXISTS `gender`;
+
+-- Dumping database structure for fur
+CREATE DATABASE IF NOT EXISTS `fur` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `fur`;
+
+-- Dumping structure for table fur.gender
 CREATE TABLE IF NOT EXISTS `gender` (
   `genderId` tinyint(1) NOT NULL AUTO_INCREMENT,
   `gender` varchar(50) DEFAULT NULL,
@@ -40,13 +29,9 @@ CREATE TABLE IF NOT EXISTS `gender` (
   UNIQUE KEY `gender` (`gender`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
+-- Dumping structure for table fur.roles
 CREATE TABLE IF NOT EXISTS `roles` (
   `roleId` tinyint(1) NOT NULL AUTO_INCREMENT,
   `role` varchar(50) DEFAULT NULL,
@@ -56,13 +41,9 @@ CREATE TABLE IF NOT EXISTS `roles` (
   UNIQUE KEY `role` (`role`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `skills`
---
-
-DROP TABLE IF EXISTS `skills`;
+-- Dumping structure for table fur.skills
 CREATE TABLE IF NOT EXISTS `skills` (
   `skillId` bigint(11) NOT NULL AUTO_INCREMENT,
   `skill` varchar(200) DEFAULT NULL,
@@ -73,13 +54,9 @@ CREATE TABLE IF NOT EXISTS `skills` (
   UNIQUE KEY `skill` (`skill`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
+-- Dumping structure for table fur.users
 CREATE TABLE IF NOT EXISTS `users` (
   `userId` bigint(11) NOT NULL AUTO_INCREMENT,
   `fullname` varchar(50) DEFAULT NULL,
@@ -91,65 +68,50 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`userId`),
   UNIQUE KEY `email` (`email`),
   KEY `users_ibfk_1` (`genderId`),
-  KEY `users_ibfk_2` (`roleId`)
+  KEY `users_ibfk_2` (`roleId`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`genderId`) REFERENCES `gender` (`genderId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `roles` (`roleId`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `user_points`
---
-
-DROP TABLE IF EXISTS `user_points`;
+-- Dumping structure for table fur.user_points
 CREATE TABLE IF NOT EXISTS `user_points` (
   `userId` bigint(11) NOT NULL,
   `points` double(8,2) NOT NULL DEFAULT 0.00,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`userId`,`points`)
+  PRIMARY KEY (`userId`,`points`),
+  CONSTRAINT `user_points_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `user_skills`
---
-
-DROP TABLE IF EXISTS `user_skills`;
+-- Dumping structure for table fur.user_skills
 CREATE TABLE IF NOT EXISTS `user_skills` (
   `userId` bigint(11) NOT NULL,
   `skillId` bigint(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`userId`,`skillId`),
-  KEY `user_skills_ibfk_2` (`skillId`)
+  KEY `user_skills_ibfk_2` (`skillId`),
+  CONSTRAINT `user_skills_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION,
+  CONSTRAINT `user_skills_ibfk_2` FOREIGN KEY (`skillId`) REFERENCES `skills` (`skillId`) ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Constraints for dumped tables
---
+-- Data exporting was unselected.
 
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`genderId`) REFERENCES `gender` (`genderId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `roles` (`roleId`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- Dumping structure for trigger fur.trg_after_user_insert
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+DELIMITER //
+CREATE TRIGGER `trg_after_user_insert` AFTER INSERT ON `users` FOR EACH ROW BEGIN
+INSERT INTO user_points (userId, points) VALUES (NEW.userId, 0.00);
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
 
---
--- Constraints for table `user_points`
---
-ALTER TABLE `user_points`
-  ADD CONSTRAINT `user_points_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION;
-
---
--- Constraints for table `user_skills`
---
-ALTER TABLE `user_skills`
-  ADD CONSTRAINT `user_skills_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION,
-  ADD CONSTRAINT `user_skills_ibfk_2` FOREIGN KEY (`skillId`) REFERENCES `skills` (`skillId`) ON DELETE NO ACTION;
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
